1202eb9d7d2ca0b1fdff81d4713487543a186da4
unknown <zakaria.janah2018@gmail.com>
2024-01-23 10:59:16 +0100

    adding the second function of gradient penalty

--- gradient_penalty.py
+++ gradient_penalty.py
@@ -29,3 +29,29 @@ def test_get_gradient(image_shape):
 
 gradient = test_get_gradient((256, 1, 28, 28))
 print("Success!")
+
+
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+    gradient_norm = gradient.norm(2, dim=1)
+
+    penalty = ((1-gradient_norm)**2).mean()
+
+    return penalty
+
+def test_gradient_penalty(image_shape):
+    bad_gradient = torch.zeros(*image_shape)
+    bad_gradient_penalty = gradient_penalty(bad_gradient)
+    assert torch.isclose(bad_gradient_penalty, torch.tensor(1.))
+
+    image_size = torch.prod(torch.Tensor(image_shape[1:]))
+    good_gradient = torch.ones(*image_shape) / torch.sqrt(image_size)
+    good_gradient_penalty = gradient_penalty(good_gradient)
+    assert torch.isclose(good_gradient_penalty, torch.tensor(0.))
+
+    random_gradient = test_get_gradient(image_shape)
+    random_gradient_penalty = gradient_penalty(random_gradient)
+    assert torch.abs(random_gradient_penalty - 1) < 0.1
+
+test_gradient_penalty((256, 1, 28, 28))
+print("Success!")
